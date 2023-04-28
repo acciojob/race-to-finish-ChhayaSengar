7416f8854689242a2e53828fc5abc91c333bfa1f
Chhaya Sengar <117908142+ChhayaSengar@users.noreply.github.com>
2023-04-28 15:05:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+const promises = [
+  new Promise(resolve => setTimeout(() => resolve(Math.random() * 4 + 1), Math.random() * 4000)),
+  new Promise(resolve => setTimeout(() => resolve(Math.random() * 4 + 1), Math.random() * 4000)),
+  new Promise(resolve => setTimeout(() => resolve(Math.random() * 4 + 1), Math.random() * 4000)),
+  new Promise(resolve => setTimeout(() => resolve(Math.random() * 4 + 1), Math.random() * 4000)),
+  new Promise(resolve => setTimeout(() => resolve(Math.random() * 4 + 1), Math.random() * 4000)),
+];
+
+Promise.any(promises)
+  .then(result => {
+    document.getElementById("output").innerHTML = `The first promise to resolve returned: ${result}`;
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
